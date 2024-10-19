c85b7d1126b7b2ff79bc2057c3fae1a12e7bd2fa
Marwanmohamed <marwan.mohamed5673@gmail.com>
2024-10-20 00:22:08 +0300

    insurance providers

--- app/Filament/Resources/InsuranceProvidersResource.php
+++ app/Filament/Resources/InsuranceProvidersResource.php
@@ -4,20 +4,20 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\InsuranceProvidersResource\Pages;
 use App\Filament\Resources\InsuranceProvidersResource\RelationManagers;
-use App\Models\InsuranceProviders;
-use Filament\Forms;
+use App\Models\Company;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class InsuranceProvidersResource extends Resource
 {
-    protected static ?string $model = InsuranceProviders::class;
+    protected static ?string $model = Company::class;
+    protected static ?string $navigationIcon = 'phosphor-images';
+    protected static ?int $navigationSort = 2;
+    protected static ?string $navigationGroup = 'Settings';
+    protected static ?string $label = 'Insurance Providers';
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
@@ -31,18 +31,24 @@ class InsuranceProvidersResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\ImageColumn::make('logo')
+                    ->getStateUsing(function($record) {
+                        return url(asset('/imgs/companies/' . $record->logo));
+                    }),
+                Tables\Columns\ToggleColumn::make('active')
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+//                Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+//                Tables\Actions\BulkActionGroup::make([
+//                    Tables\Actions\DeleteBulkAction::make(),
+//                ]),
             ]);
     }
 
@@ -58,7 +64,7 @@ class InsuranceProvidersResource extends Resource
         return [
             'index' => Pages\ListInsuranceProviders::route('/'),
             'create' => Pages\CreateInsuranceProviders::route('/create'),
-            'edit' => Pages\EditInsuranceProviders::route('/{record}/edit'),
+//            'edit' => Pages\EditInsuranceProviders::route('/{record}/edit'),
         ];
     }
 }
